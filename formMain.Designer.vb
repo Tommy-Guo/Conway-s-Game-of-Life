246295a0d0069720febe8d0dd4e62f00e658c439
VmX5 <SockMonkeyboy@hotmail.com>
2017-12-12 16:25:12 -0500

    Initial Commit
    Uploaded the universe.

---
+++ formMain.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class formMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnNextStep = New System.Windows.Forms.Button()
+        Me.btnStartStep = New System.Windows.Forms.Button()
+        Me.btnStepStop = New System.Windows.Forms.Button()
+        Me.universeTime = New System.Windows.Forms.Timer(Me.components)
+        Me.numTimerValue = New System.Windows.Forms.NumericUpDown()
+        Me.btnRestart = New System.Windows.Forms.Button()
+        Me.lblSpeed = New System.Windows.Forms.Label()
+        Me.lblMS = New System.Windows.Forms.Label()
+        Me.lblGenerationCount = New System.Windows.Forms.Label()
+        Me.Universe1 = New Conways_Game_of_Life.universe()
+        CType(Me.numTimerValue, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnNextStep
+        '
+        Me.btnNextStep.Location = New System.Drawing.Point(710, 375)
+        Me.btnNextStep.Name = "btnNextStep"
+        Me.btnNextStep.Size = New System.Drawing.Size(101, 23)
+        Me.btnNextStep.TabIndex = 0
+        Me.btnNextStep.Text = "Next Generation"
+        Me.btnNextStep.UseVisualStyleBackColor = True
+        '
+        'btnStartStep
+        '
+        Me.btnStartStep.Font = New System.Drawing.Font("Consolas", 9.0!)
+        Me.btnStartStep.Location = New System.Drawing.Point(127, 376)
+        Me.btnStartStep.Name = "btnStartStep"
+        Me.btnStartStep.Size = New System.Drawing.Size(23, 23)
+        Me.btnStartStep.TabIndex = 3
+        Me.btnStartStep.Text = "►"
+        Me.btnStartStep.UseVisualStyleBackColor = True
+        '
+        'btnStepStop
+        '
+        Me.btnStepStop.Font = New System.Drawing.Font("Consolas", 9.0!)
+        Me.btnStepStop.Location = New System.Drawing.Point(156, 376)
+        Me.btnStepStop.Name = "btnStepStop"
+        Me.btnStepStop.Size = New System.Drawing.Size(23, 23)
+        Me.btnStepStop.TabIndex = 4
+        Me.btnStepStop.Text = "■"
+        Me.btnStepStop.UseVisualStyleBackColor = True
+        '
+        'universeTime
+        '
+        Me.universeTime.Interval = 300
+        '
+        'numTimerValue
+        '
+        Me.numTimerValue.Location = New System.Drawing.Point(46, 378)
+        Me.numTimerValue.Maximum = New Decimal(New Integer() {1316134912, 2328, 0, 0})
+        Me.numTimerValue.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.numTimerValue.Name = "numTimerValue"
+        Me.numTimerValue.Size = New System.Drawing.Size(54, 20)
+        Me.numTimerValue.TabIndex = 5
+        Me.numTimerValue.Value = New Decimal(New Integer() {50, 0, 0, 0})
+        '
+        'btnRestart
+        '
+        Me.btnRestart.Location = New System.Drawing.Point(629, 375)
+        Me.btnRestart.Name = "btnRestart"
+        Me.btnRestart.Size = New System.Drawing.Size(75, 23)
+        Me.btnRestart.TabIndex = 6
+        Me.btnRestart.Text = "Restart"
+        Me.btnRestart.UseVisualStyleBackColor = True
+        '
+        'lblSpeed
+        '
+        Me.lblSpeed.AutoSize = True
+        Me.lblSpeed.Location = New System.Drawing.Point(7, 380)
+        Me.lblSpeed.Name = "lblSpeed"
+        Me.lblSpeed.Size = New System.Drawing.Size(41, 13)
+        Me.lblSpeed.TabIndex = 7
+        Me.lblSpeed.Text = "Speed:"
+        '
+        'lblMS
+        '
+        Me.lblMS.AutoSize = True
+        Me.lblMS.Location = New System.Drawing.Point(101, 380)
+        Me.lblMS.Name = "lblMS"
+        Me.lblMS.Size = New System.Drawing.Size(20, 13)
+        Me.lblMS.TabIndex = 8
+        Me.lblMS.Text = "ms"
+        '
+        'lblGenerationCount
+        '
+        Me.lblGenerationCount.Location = New System.Drawing.Point(434, 380)
+        Me.lblGenerationCount.Name = "lblGenerationCount"
+        Me.lblGenerationCount.Size = New System.Drawing.Size(189, 13)
+        Me.lblGenerationCount.TabIndex = 9
+        Me.lblGenerationCount.Text = "Universe Generations: 0"
+        Me.lblGenerationCount.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Universe1
+        '
+        Me.Universe1.aliveColor = System.Drawing.Color.Black
+        Me.Universe1.cellSize = New System.Drawing.Size(5, 5)
+        Me.Universe1.Location = New System.Drawing.Point(10, 12)
+        Me.Universe1.Name = "Universe1"
+        Me.Universe1.Size = New System.Drawing.Size(801, 356)
+        Me.Universe1.TabIndex = 10
+        Me.Universe1.Text = "Universe1"
+        Me.Universe1.universeSize = New System.Drawing.Size(160, 71)
+        '
+        'formMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(823, 404)
+        Me.Controls.Add(Me.Universe1)
+        Me.Controls.Add(Me.lblGenerationCount)
+        Me.Controls.Add(Me.numTimerValue)
+        Me.Controls.Add(Me.lblMS)
+        Me.Controls.Add(Me.lblSpeed)
+        Me.Controls.Add(Me.btnRestart)
+        Me.Controls.Add(Me.btnStepStop)
+        Me.Controls.Add(Me.btnStartStep)
+        Me.Controls.Add(Me.btnNextStep)
+        Me.Name = "formMain"
+        Me.Text = "Conway's Game of Life"
+        CType(Me.numTimerValue, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnNextStep As System.Windows.Forms.Button
+    Friend WithEvents btnStartStep As System.Windows.Forms.Button
+    Friend WithEvents btnStepStop As System.Windows.Forms.Button
+    Friend WithEvents universeTime As System.Windows.Forms.Timer
+    Friend WithEvents numTimerValue As System.Windows.Forms.NumericUpDown
+    Friend WithEvents btnRestart As System.Windows.Forms.Button
+    Friend WithEvents lblSpeed As System.Windows.Forms.Label
+    Friend WithEvents lblMS As System.Windows.Forms.Label
+    Friend WithEvents lblGenerationCount As System.Windows.Forms.Label
+    Friend WithEvents Universe1 As Conways_Game_of_Life.universe
+
+End Class
---
+++ formMain.vb
@@ -0,0 +1,221 @@
+﻿Public Class formMain
+
+    Private Sub btnNextStep_Click(sender As Object, e As EventArgs) Handles btnNextStep.Click
+        'We will tell the universe to go to the next generation.
+        Universe1.nextGeneration()
+        lblGenerationCount.Text = String.Format("Universe Generations: {0}", Universe1.Generations)
+    End Sub
+
+    Private Sub btnStartStep_Click(sender As Object, e As EventArgs) Handles btnStartStep.Click
+        universeTime.Start()
+    End Sub
+
+    Private Sub btnStepStop_Click(sender As Object, e As EventArgs) Handles btnStepStop.Click
+        universeTime.Stop()
+    End Sub
+
+    Private Sub stepTimer_Tick(sender As Object, e As EventArgs) Handles universeTime.Tick
+        'The timer will tell the universe to go to the next generation every time it executes.
+        Universe1.nextGeneration()
+        lblGenerationCount.Text = String.Format("Universe Generations: {0}", Universe1.Generations)
+    End Sub
+
+    Private Sub numTimerValue_ValueChanged(sender As Object, e As EventArgs) Handles numTimerValue.ValueChanged
+        'This numeric control controls the rate at which the universe goes to the next generation.
+        universeTime.Interval = numTimerValue.Value
+    End Sub
+
+    Private Sub btnRestart_Click(sender As Object, e As EventArgs) Handles btnRestart.Click
+        'By populating the arrays we assign each cell it's location and it's status as dead.
+        Universe1.clearArrays()
+        universeTime.Stop()
+    End Sub 
+End Class
+
+Public Class universe
+    Inherits Control
+
+#Region "Properties"
+    Public Property cellSize As New Size(5, 5)
+    Public Property aliveColor As Color = Color.Black
+    Private _generations As Integer = 0
+    Public ReadOnly Property Generations As Integer
+        Get
+            Return _generations
+        End Get
+    End Property
+    Private _universeSize As New Size(60, 30)
+    Public Property universeSize As Size
+        Get
+            Return _universeSize
+        End Get
+        Set(value As Size)
+            _universeSize = value
+            ReDim cells(_universeSize.Width - 1, _universeSize.Height - 1)
+            Invalidate()
+            clearArrays()
+        End Set
+    End Property
+#End Region
+
+    Private cells(_universeSize.Width - 1, _universeSize.Height - 1) As cell
+
+    Sub New()
+        DoubleBuffered = True
+        clearArrays()
+    End Sub
+
+    Public Sub clearArrays()
+        _generations = 0
+        For y As Integer = 0 To _universeSize.Height - 1
+            For x As Integer = 0 To _universeSize.Width - 1
+                cells(x, y) = New cell(New Point(x, y))
+            Next
+        Next
+    End Sub
+
+    Protected Overrides Sub OnPaint(e As PaintEventArgs)
+        MyBase.OnPaint(e)
+        Dim g As Graphics = e.Graphics : e.Graphics.SmoothingMode = Drawing2D.SmoothingMode.HighSpeed
+
+
+        For Each cell As cell In cells
+            'Update the cells that should be dead or alive after last generation.
+            If cell.shouldToggle Then
+                cell.shouldToggle = False
+                toggleCell(cell)
+            End If
+        Next
+
+        For Each cell As cell In cells
+            'Draw cells that are alive in selected colour.
+            If cell.status = status.Alive Then
+                g.FillRectangle(New SolidBrush(aliveColor), New Rectangle(cell.location.X * cellSize.Width, cell.location.Y * cellSize.Height, cellSize.Width, cellSize.Height))
+            End If
+        Next
+
+        'Draw grid, dependent on universe size.
+        For x As Integer = 0 To _universeSize.Width
+            g.DrawLine(Pens.Gray, x * cellSize.Width, 0, x * cellSize.Width, Height)
+        Next
+        For y As Integer = 0 To _universeSize.Height
+            g.DrawLine(Pens.Gray, 0, y * cellSize.Height, Width, y * cellSize.Height)
+        Next
+
+        'Draw border on entire control.
+        g.DrawRectangle(Pens.Gray, 0, 0, Width - 1, Height - 1)
+    End Sub
+
+    Public Sub nextGeneration()
+        For y As Integer = 0 To _universeSize.Height - 1
+            For x As Integer = 0 To _universeSize.Width - 1
+                updateCell(cells(x, y))
+            Next
+        Next
+        _generations += 1
+        Invalidate()
+    End Sub
+
+    Private Sub updateCell(cell As cell)
+        Dim aliveNeighbours As Integer = getNeighbourhood(cell)
+        If cell.status = status.Alive And aliveNeighbours < 2 Then
+            cells(cell.location.X, cell.location.Y).shouldToggle = True
+        ElseIf cell.status = status.Alive And (aliveNeighbours = 2 Or aliveNeighbours = 3) Then
+            cells(cell.location.X, cell.location.Y).shouldToggle = False
+        ElseIf cell.status = status.Alive And aliveNeighbours > 3 Then
+            cells(cell.location.X, cell.location.Y).shouldToggle = True
+        ElseIf cell.status = status.Dead And aliveNeighbours = 3 Then
+            cells(cell.location.X, cell.location.Y).shouldToggle = True
+        End If
+    End Sub
+
+    Public Function getNeighbourhood(sourceCell As cell) As Integer
+        Dim neighbourCount As Integer = 0
+        'North
+        If getCellStatus(sourceCell.location.X, sourceCell.location.Y - 1) = status.Alive Then neighbourCount += 1
+        'North East
+        If getCellStatus(sourceCell.location.X + 1, sourceCell.location.Y - 1) = status.Alive Then neighbourCount += 1
+        'East
+        If getCellStatus(sourceCell.location.X + 1, sourceCell.location.Y) = status.Alive Then neighbourCount += 1
+        'South East
+        If getCellStatus(sourceCell.location.X + 1, sourceCell.location.Y + 1) = status.Alive Then neighbourCount += 1
+        'South
+        If getCellStatus(sourceCell.location.X, sourceCell.location.Y + 1) = status.Alive Then neighbourCount += 1
+        'South West
+        If getCellStatus(sourceCell.location.X - 1, sourceCell.location.Y + 1) = status.Alive Then neighbourCount += 1
+        'West
+        If getCellStatus(sourceCell.location.X - 1, sourceCell.location.Y) = status.Alive Then neighbourCount += 1
+        'North West
+        If getCellStatus(sourceCell.location.X - 1, sourceCell.location.Y - 1) = status.Alive Then neighbourCount += 1
+        Return neighbourCount
+    End Function
+
+    Private Function getCellStatus(x As Integer, y As Integer) As status
+        Return cells(toroidalArray(x, y).X, toroidalArray(x, y).Y).status
+    End Function
+
+    Private Function toroidalArray(x As Integer, y As Integer) As Point
+        Dim outputX, outputY As Integer
+        If x > -1 Then
+            outputX = x Mod _universeSize.Width
+        Else
+            outputX = _universeSize.Width - Math.Abs(x)
+        End If
+        If y > -1 Then
+            outputY = y Mod _universeSize.Height
+        Else
+            outputY = _universeSize.Height - Math.Abs(y)
+        End If
+        Return New Point(outputX, outputY)
+    End Function
+
+    Protected Overrides Sub OnMouseMove(e As MouseEventArgs)
+        MyBase.OnMouseMove(e)
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            Try
+                changeCell(cells(Math.Floor(e.X / cellSize.Width), Math.Floor(e.Y / cellSize.Height)), status.Alive)
+            Catch
+                'The selected cell does not exist.
+            End Try
+        ElseIf e.Button = Windows.Forms.MouseButtons.Right Then
+            Try
+                changeCell(cells(Math.Floor(e.X / cellSize.Width), Math.Floor(e.Y / cellSize.Height)), status.Dead)
+            Catch
+                'The selected cell does not exist.
+            End Try
+        End If
+        Invalidate()
+    End Sub
+
+    Private Sub changeCell(sourceCell As cell, toStats As status)
+        sourceCell.status = toStats
+    End Sub
+
+    Private Sub toggleCell(cell As cell)
+        cell.status = IIf(cell.status = status.Alive, status.Dead, status.Alive)
+    End Sub
+
+    Protected Overrides Sub OnMouseDown(e As MouseEventArgs)
+        MyBase.OnMouseDown(e)
+        Select Case e.Button
+            Case Windows.Forms.MouseButtons.Left
+                OnMouseMove(New MouseEventArgs(Windows.Forms.MouseButtons.Left, 1, e.X, e.Y, e.Delta))
+            Case Right
+                OnMouseMove(New MouseEventArgs(Windows.Forms.MouseButtons.Right, 1, e.X, e.Y, e.Delta))
+        End Select
+    End Sub
+End Class
+
+Public Class cell
+    Public status As status = status.Dead
+    Public location As Point
+    Public shouldToggle As Boolean = False
+    Sub New(x As Point)
+        Me.location = x
+    End Sub
+End Class
+
+Public Enum status
+    Dead = 1
+    Alive = 2
+End Enum
